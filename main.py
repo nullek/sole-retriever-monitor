4ec7f05f649776a06061ffe117684e729d3353d2
nullek <65280991+nullek@users.noreply.github.com>
2021-04-25 23:34:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,42 +6,44 @@ import discord
 import asyncio
 
 client = discord.Client()
-with open("links.json","r") as f:
+with open("links.json", "r") as f:
     links = json.load(f)
+
 
 @client.event
 async def on_ready():
-  while True:
-    channel = client.get_channel(here paste channel ID)
-    req = requests.get('here paste link to raffle') #for example: https://www.soleretriever.com/raffles/nike-vaporwaffle-sacai-tan-navy-dd1875-200
-    soup = BeautifulSoup(req.text, "html.parser")
+    while True:
+        channel = client.get_channel(here paste channel ID)
+        req = requests.get('here paste link to raffle') #for example: https://www.soleretriever.com/raffles/nike-vaporwaffle-sacai-tan-navy-dd1875-200
+        soup = BeautifulSoup(req.text, "html.parser")
 
-    for main in soup.find_all("div", class_="sc-bdfBwQ sc-gsTCUz zanrti-0 jzXtZT bhdLno cYEQVO"):
-        shoeName = main.find("h1", color="text").text
-        shoeImage = main.find("img").get("src")
+        for a in soup.find_all("img", class_="zanrti-3 jxsqzH"):
+            shoeName = a.get("alt")
+            shoeImage = a.get("src")
 
-        for img in soup.find_all("div", alt="Open raffle"):
-            link = img.find("a", target="_blank").get("href")
-            site = img.find("img").get("alt")
-            image = img.find("img").get("src")
-            ends = img.find("div", color="darkGray").text
-            raffleType = img.find("div", class_="sc-bdfBwQ sc-gsTCUz jzXtZT bhdLno").find_all("div")[0].text
-            region = img.find("div", class_="sc-bdfBwQ sc-gsTCUz jzXtZT bhdLno").find_all("div")[1].text
-            if link not in links:
-                links.append(link)
-                embed = discord.Embed(title=shoeName , url=link , timestamp=datetime.datetime.utcnow(), color=0x5377e9)
-                embed.set_thumbnail(url=image)
-                embed.add_field(name="Ends:", value=ends)
-                embed.add_field(name="Site:", value=site, inline=False)
-                embed.add_field(name="Type:", value=raffleType, inline=False)
-                embed.add_field(name="Region:", value=region, inline=False)
-                embed.set_image(url=shoeImage)
-                embed.set_footer(text="Made by nullek#4646 | @nullek2")
-                msg = await channel.send(embed=embed)
-                await msg.add_reaction("✅")
-                with open("links.json","w") as f:
-                    json.dump(links,f)   
+            for img in soup.find_all("div", alt="Open raffle"):
+                link = img.find("a", target="_blank").get("href")
+                site = img.find("img").get("alt")
+                image = img.find("img").get("src")
+                ends = img.find("div", color="darkGray").text
+                raffleType = img.find("div", class_="sc-bdvvaa sc-gsDJrp itMAzf jXLjZO").find_all("div")[0].text
+                region = img.find("div", class_="sc-bdvvaa sc-gsDJrp itMAzf jXLjZO").find_all("div")[1].text
+        
+                if link not in links:
+                    links.append(link)
+                    embed = discord.Embed(title=shoeName, url=link, timestamp=datetime.datetime.utcnow(), color=0x5377e9)
+                    embed.set_thumbnail(url=image)
+                    embed.add_field(name="Ends:", value=ends)
+                    embed.add_field(name="Site:", value=site, inline=False)
+                    embed.add_field(name="Type:", value=raffleType, inline=False)
+                    embed.add_field(name="Region:", value=region, inline=False)
+                    embed.set_image(url=shoeImage)
+                    embed.set_footer(text="Made by nullek#4646 | @nullek2")
+                    msg = await channel.send(embed=embed)
+                    await msg.add_reaction("✅")
+                    with open("links.json","w") as f:
+                        json.dump(links,f)
 
-    await asyncio.sleep(60*5)
+    await asyncio.sleep(60*10)
 
 client.run("here paste your bot token")
